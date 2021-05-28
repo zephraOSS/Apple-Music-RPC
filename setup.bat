3344d70f68c0d3d44dc49297aaeed3f6f1c6025c
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-05-28 16:04:40 +0200

    💥 No longer needed

--- setup.bat
+++
@@ -1,9 +0,0 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/N0chteil/Apple-Music-RPC/releases/download/v1.0.2/amrpc-win.zip', 'amrpc-win.zip')"
-powershell -Command "Expand-Archive amrpc-win.zip ."
-del "amrpc-win.zip"
->AMRPC.bat (
-  echo npm start
-)
-npm install
-npm start setup
-exit 0
