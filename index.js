49488f657fb418a287c42bab16dbf5f474a2beb6
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-10-22 17:21:12 +0200

    🏆 Pretty code

--- index.js
+++ index.js
@@ -12,24 +12,28 @@ const clientId = "842112189618978897",
     fs = require("fs");
 
 const iTunesEmitter = iTunes.emitter,
-    config = new Store({defaults: {
-        autolaunch: true,
-        show: true,
-        hideOnPause: true,
-        showAlbumCover: true,
-        performanceMode: false,
-        colorTheme: "white",
-        language: "en_US",
-        cover: "applemusic-logo",
-        rpcDetails: "%title% - %album%",
-        rpcState: "%artist%"
-    }}),
-    appData = new Store({name: "data", defaults: {
-        userCountUsageAsked: false,
-        nineelevenAsked: false,
-        appleEventAsked: false,
-        nineelevenCovers: false
-    }});
+    config = new Store({
+        defaults: {
+            autolaunch: true,
+            show: true,
+            hideOnPause: true,
+            showAlbumCover: true,
+            performanceMode: false,
+            colorTheme: "white",
+            language: "en_US",
+            cover: "applemusic-logo",
+            rpcDetails: "%title% - %album%",
+            rpcState: "%artist%"
+        }
+    }),
+    appData = new Store({
+        name: "data", defaults: {
+            userCountUsageAsked: false,
+            nineelevenAsked: false,
+            appleEventAsked: false,
+            nineelevenCovers: false
+        }
+    });
 
 console.log = log.log;
 app.dev = (app.isPackaged) ? false : true;
@@ -45,8 +49,8 @@ let rpc = new DiscordRPC.Client({ transport: "ipc" }),
     ctG,
     disconnected = false;
 
-require("child_process").exec("NET SESSION", function(err,so,se) {
-    if(se.length === 0) {
+require("child_process").exec("NET SESSION", function (err, so, se) {
+    if (se.length === 0) {
         isQuiting = true;
         console.log("Please do not run AMRPC with administrator privileges!");
         dialog.showErrorBox("Oh no!", langString.error.admin);
@@ -54,19 +58,19 @@ require("child_process").exec("NET SESSION", function(err,so,se) {
     }
 });
 
-iTunesEmitter.on("playing", async function(type, currentTrack) {
-    if(!currentTrack) return console.log("No track detected");
+iTunesEmitter.on("playing", async function (type, currentTrack) {
+    if (!currentTrack) return console.log("No track detected");
     ctG = currentTrack;
 
-    if(currentTrack.album === 0) presenceData.details = currentTrack.name;
+    if (currentTrack.album === 0) presenceData.details = currentTrack.name;
     else replaceRPCVars(currentTrack, "rpcDetails");
 
     replaceRPCVars(currentTrack, "rpcState");
 
-    if(currentTrack.duration > 0)
+    if (currentTrack.duration > 0)
         presenceData.endTimestamp = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + currentTrack.duration;
     else {
-        if(presenceData.endTimestamp) delete presenceData.endTimestamp;
+        if (presenceData.endTimestamp) delete presenceData.endTimestamp;
         presenceData.details = currentTrack.name;
         presenceData.state = "LIVE";
     }
@@ -80,7 +84,7 @@ iTunesEmitter.on("playing", async function(type, currentTrack) {
     console.log("currentTrack.album", currentTrack.album);
     console.log("timestamp", Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + currentTrack.duration);
 
-    getAppleMusicData(currentTrack.name, currentTrack.artist, function(res, err) {
+    getAppleMusicData(currentTrack.name, currentTrack.artist, function (res, err) {
         if (!err) {
             console.log("currentTrack url", res.url);
             presenceData.buttons = [
@@ -90,18 +94,18 @@ iTunesEmitter.on("playing", async function(type, currentTrack) {
                 }
             ]
             rpc.setActivity(presenceData);
-        } else if(presenceData.buttons) delete presenceData.buttons;
+        } else if (presenceData.buttons) delete presenceData.buttons;
     });
 
     rpc.setActivity(presenceData);
 });
 
-iTunesEmitter.on("paused", async function(type, currentTrack) {
-    if(!currentTrack) return console.log("No track detected");
+iTunesEmitter.on("paused", async function (type, currentTrack) {
+    if (!currentTrack) return console.log("No track detected");
     ctG = currentTrack;
 
-    if(config.get("hideOnPause")) {
-        if(presenceData.details || presenceData.state || presenceData.endTimestamp || presenceData.buttons) rpc.clearActivity();
+    if (config.get("hideOnPause")) {
+        if (presenceData.details || presenceData.state || presenceData.endTimestamp || presenceData.buttons) rpc.clearActivity();
         delete presenceData.details;
         delete presenceData.state;
         delete presenceData.endTimestamp;
@@ -123,7 +127,7 @@ iTunesEmitter.on("paused", async function(type, currentTrack) {
 
 iTunesEmitter.on("stopped", async () => {
     console.log("action", "stopped");
-    if(presenceData.details || presenceData.state || presenceData.endTimestamp || presenceData.buttons) rpc.clearActivity();
+    if (presenceData.details || presenceData.state || presenceData.endTimestamp || presenceData.buttons) rpc.clearActivity();
     delete presenceData.details;
     delete presenceData.state;
     delete presenceData.endTimestamp;
@@ -131,7 +135,7 @@ iTunesEmitter.on("stopped", async () => {
     ctG = undefined;
 });
 
-if(!config.get("performanceMode")) {
+if (!config.get("performanceMode")) {
     iTunesEmitter.on("timeChange", async (type, currentTrack) => {
         if (!presenceData.endTimestamp) return;
         let ct = Math.floor(Date.now() / 1000) - currentTrack.elapsedTime + currentTrack.duration;
@@ -151,13 +155,13 @@ rpc.on("ready", () => {
     disconnected = false;
     ctG = iTunes.getCurrentTrack();
 
-    if(ctG && ctG.playerState === "playing") {
-        if(ctG.album.length === 0) presenceData.details = ctG.name;
+    if (ctG && ctG.playerState === "playing") {
+        if (ctG.album.length === 0) presenceData.details = ctG.name;
         else replaceRPCVars(ctG, "rpcDetails");
 
         replaceRPCVars(ctG, "rpcState");
 
-        if(ctG.duration === 0) {
+        if (ctG.duration === 0) {
             presenceData.details = ctG.name;
             presenceData.state = "LIVE";
         }
@@ -172,9 +176,9 @@ rpc.on("ready", () => {
 
 rpc.on("disconnected", () => {
     disconnected = true;
-    
+
     const interval = setInterval(() => {
-        if(!disconnected) return clearInterval(interval);
+        if (!disconnected) return clearInterval(interval);
         rpc?.destroy();
 
         rpc = new DiscordRPC.Client({ transport: "ipc" });
@@ -184,22 +188,22 @@ rpc.on("disconnected", () => {
             disconnected = false;
             ctG = iTunes.getCurrentTrack();
 
-            if(ctG && ctG.playerState === "playing") {
-                if(ctG.album.length === 0) presenceData.details = ctG.name;
+            if (ctG && ctG.playerState === "playing") {
+                if (ctG.album.length === 0) presenceData.details = ctG.name;
                 else replaceRPCVars(ctG, "rpcDetails");
-        
+
                 replaceRPCVars(ctG, "rpcState");
-        
-                if(ctG.duration === 0) {
+
+                if (ctG.duration === 0) {
                     presenceData.details = ctG.name;
                     presenceData.state = "LIVE";
                 }
 
                 rpc.setActivity(presenceData);
-        
+
                 checkCover(ctG);
             }
-        
+
             startCheckInterval();
         });
     }, 1000);
@@ -223,7 +227,7 @@ app.on("ready", () => {
             { label: langString.tray.openSettings, click() { mainWindow.show() } },
             { type: "separator" },
             { label: langString.tray.quit, click() { isQuiting = true, app.quit() } }
-          ]);
+        ]);
 
     app.on("quit", () => tray.destroy());
     app.on("before-quit", function () {
@@ -235,7 +239,7 @@ app.on("ready", () => {
     tray.on("right-click", () => tray.update());
     tray.on("click", () => mainWindow.show());
 
-    if(config.get("autolaunch")) autoLaunch.enable();
+    if (config.get("autolaunch")) autoLaunch.enable();
     else autoLaunch.disable();
 
     mainWindow = new BrowserWindow({
@@ -253,12 +257,12 @@ app.on("ready", () => {
         pathname: path.join(__dirname),
         protocol: "file:",
         slashes: true
-    })+"/index.html");
+    }) + "/index.html");
 
     require("@electron/remote/main").initialize();
 
-    mainWindow.on("close", function(event) {
-        if(!isQuiting) {
+    mainWindow.on("close", function (event) {
+        if (!isQuiting) {
             event.preventDefault();
             mainWindow.hide();
             return false;
@@ -295,7 +299,7 @@ app.on("ready", () => {
             }
         });
     });
-    
+
     autoUpdater.on('update-downloaded', (info) => {
         sendMsgToMainWindow("asynchronous-message", 'Update downloaded');
         autoUpdater.quitAndInstall();
@@ -326,16 +330,16 @@ function updateChecker() {
 
     autoUpdater.checkForUpdatesAndNotify();
 
-    if(!app.isPackaged) return;
+    if (!app.isPackaged) return;
 
-    fetch("https://raw.githubusercontent.com/N0chteil-Productions/Apple-Music-RPC/main/covers.json", {cache: "no-store"}, function(error, meta, body) {
-        if(!body) return console.log(`Error ${error}. Cover check was canceled.`);
+    fetch("https://raw.githubusercontent.com/N0chteil-Productions/Apple-Music-RPC/main/covers.json", { cache: "no-store" }, function (error, meta, body) {
+        if (!body) return console.log(`Error ${error}. Cover check was canceled.`);
         body = JSON.parse(body.toString());
 
         console.log("Checking for new covers...");
-        
-        if(!isEqual(require("./covers.json"), body)) {
-            fs.writeFile(path.join(app.isPackaged ? process.resourcesPath + "/app.asar.unpacked" : __dirname, "/covers.json"), JSON.stringify(body, null, 4), function (err) {if (err) console.log(err)});
+
+        if (!isEqual(require("./covers.json"), body)) {
+            fs.writeFile(path.join(app.isPackaged ? process.resourcesPath + "/app.asar.unpacked" : __dirname, "/covers.json"), JSON.stringify(body, null, 4), function (err) { if (err) console.log(err) });
             console.log("Updated covers");
             showNotification("AMRPC", langString.notification.coverlistUpdated);
             setTimeout(() => {
@@ -349,22 +353,22 @@ function updateChecker() {
 function languageChecker() {
     console.log("Checking for language changes...");
 
-    if(config.get("language") !== userLang) userLang = config.get("language");
+    if (config.get("language") !== userLang) userLang = config.get("language");
 }
 
 function showNotification(title, body) {
-    new Notification({title: title, body: body, icon: path.join(app.isPackaged ? process.resourcesPath : __dirname, "/assets/logo.png")}).show();
+    new Notification({ title: title, body: body, icon: path.join(app.isPackaged ? process.resourcesPath : __dirname, "/assets/logo.png") }).show();
 }
 
 function reGetCT(type) {
     let ct = iTunes.getCurrentTrack();
-    if(!ct || ct.playerState === "stopped") return console.log("No track detected");
+    if (!ct || ct.playerState === "stopped") return console.log("No track detected");
 
-    if(ct.album.length === 0) presenceData.details = ct.name;
+    if (ct.album.length === 0) presenceData.details = ct.name;
     else replaceRPCVars(ct, "rpcDetails");
 
     replaceRPCVars(ct, "rpcState");
-    if(ct.duration > 0) presenceData.endTimestamp = Math.floor(Date.now() / 1000) - ct.elapsedTime + ct.duration;
+    if (ct.duration > 0) presenceData.endTimestamp = Math.floor(Date.now() / 1000) - ct.elapsedTime + ct.duration;
 
     checkCover(ct);
 
@@ -374,7 +378,7 @@ function reGetCT(type) {
     console.log("currentTrack.album", ct.album);
     console.log("timestamp", Math.floor(Date.now() / 1000) - ct.elapsedTime + ct.duration);
 
-    getAppleMusicData(ct.name, ct.artist, function(res, err) {
+    getAppleMusicData(ct.name, ct.artist, function (res, err) {
         if (!err) {
             console.log("currentTrack url", res.url);
             presenceData.buttons = [
@@ -384,21 +388,21 @@ function reGetCT(type) {
                 }
             ]
             rpc.setActivity(presenceData);
-        } else if(presenceData.buttons) delete presenceData.buttons;
+        } else if (presenceData.buttons) delete presenceData.buttons;
     });
 
     rpc.setActivity(presenceData);
 }
-  
+
 function updateShowRPC(status) {
-    if(status) reGetCT("update_cfg_show");
+    if (status) reGetCT("update_cfg_show");
     else {
         rpc.clearActivity();
         delete presenceData.details;
         delete presenceData.state;
         delete presenceData.endTimestamp;
     }
-    
+
     config.set("show", status);
 }
 
@@ -406,12 +410,12 @@ function isEqual(obj1, obj2) {
     const obj1Keys = Object.keys(obj1),
         obj2Keys = Object.keys(obj2);
 
-    if(obj1Keys.length !== obj2Keys.length) return false;
+    if (obj1Keys.length !== obj2Keys.length) return false;
 
     for (let objKey of obj1Keys) {
         if (obj1[objKey] !== obj2[objKey]) {
-            if(typeof obj1[objKey] == "object" && typeof obj2[objKey] == "object") {
-                if(!isEqual(obj1[objKey], obj2[objKey])) return false;
+            if (typeof obj1[objKey] == "object" && typeof obj2[objKey] == "object") {
+                if (!isEqual(obj1[objKey], obj2[objKey])) return false;
             }
             else return false;
         }
@@ -421,33 +425,33 @@ function isEqual(obj1, obj2) {
 }
 
 function checkCover(ct) {
-    if(!ct || ct.playerState === "stopped") return;
-    if(!config.get("showAlbumCover")) return presenceData.largeImageKey = config.get("cover");
-    if(appData.get("nineelevenCovers") && (new Date().getMonth() + 1 === 9 && new Date().getDate() === 11)) return presenceData.largeImageKey = "cover_911";
+    if (!ct || ct.playerState === "stopped") return;
+    if (!config.get("showAlbumCover")) return presenceData.largeImageKey = config.get("cover");
+    if (appData.get("nineelevenCovers") && (new Date().getMonth() + 1 === 9 && new Date().getDate() === 11)) return presenceData.largeImageKey = "cover_911";
 
-    if(covers.album[ct.album.toLowerCase()]) presenceData.largeImageKey = covers.album[ct.album.toLowerCase()];
-    else if(covers.song[ct.artist.toLowerCase()]) {
-        if(covers.song[ct.artist.toLowerCase()][ct.name.toLowerCase()]) presenceData.largeImageKey = covers.song[ct.artist.toLowerCase()][ct.name.toLowerCase()];
+    if (covers.album[ct.album.toLowerCase()]) presenceData.largeImageKey = covers.album[ct.album.toLowerCase()];
+    else if (covers.song[ct.artist.toLowerCase()]) {
+        if (covers.song[ct.artist.toLowerCase()][ct.name.toLowerCase()]) presenceData.largeImageKey = covers.song[ct.artist.toLowerCase()][ct.name.toLowerCase()];
         else presenceData.largeImageKey = config.get("cover");
-    } else if(presenceData.largeImageKey !== config.get("cover")) presenceData.largeImageKey = config.get("cover");
+    } else if (presenceData.largeImageKey !== config.get("cover")) presenceData.largeImageKey = config.get("cover");
 
     rpc.setActivity(presenceData);
 }
 
 function replaceRPCVars(ct, cfg) {
-    if(!ct || !cfg || ct.playerState === "stopped") return;
+    if (!ct || !cfg || ct.playerState === "stopped") return;
 
-    if(cfg === "rpcDetails") presenceData.details = config.get(cfg).replace("%title%", ct.name).replace("%album%", ct.album).replace("%artist%", ct.artist);
-    else if(cfg === "rpcState") presenceData.state = config.get(cfg).replace("%title%", ct.name).replace("%album%", ct.album).replace("%artist%", ct.artist);
+    if (cfg === "rpcDetails") presenceData.details = config.get(cfg).replace("%title%", ct.name).replace("%album%", ct.album).replace("%artist%", ct.artist);
+    else if (cfg === "rpcState") presenceData.state = config.get(cfg).replace("%title%", ct.name).replace("%album%", ct.album).replace("%artist%", ct.artist);
 }
 
 function getAppleMusicData(title, artist, callback) {
-    const reqParam = encodeURIComponent(`${title} ${artist}`).replace(/'/g,"%27").replace(/"/g,"%22");
+    const reqParam = encodeURIComponent(`${title} ${artist}`).replace(/'/g, "%27").replace(/"/g, "%22");
 
-    fetch(`https://itunes.apple.com/search?term=${reqParam}&entity=musicTrack`, {cache: "no-store"}, function(error, meta, body) {
-        if(!body) return callback(null, true);
+    fetch(`https://itunes.apple.com/search?term=${reqParam}&entity=musicTrack`, { cache: "no-store" }, function (error, meta, body) {
+        if (!body) return callback(null, true);
         const res = JSON.parse(body.toString()).results[0];
-        if(res) callback({
+        if (res) callback({
             url: res.trackViewUrl,
             collectionId: res.collectionId,
             trackId: res.trackId,
@@ -459,21 +463,21 @@ function getAppleMusicData(title, artist, callback) {
 
 function startCheckInterval() {
     disconnected = false;
-    if(config.get("performanceMode")) {
+    if (config.get("performanceMode")) {
         const interval = setInterval(() => {
-            if(disconnected) return clearInterval(interval);
-            if(!presenceData.details) return rpc.clearActivity();
-            if(presenceData.details.length > 128) presenceData.details = presenceData.details.substring(0,128);
-            if(presenceData.state?.length > 128) presenceData.state = presenceData.state.substring(0,128);
-            else if(presenceData.state?.length === 0) delete presenceData.state;
+            if (disconnected) return clearInterval(interval);
+            if (!presenceData.details) return rpc.clearActivity();
+            if (presenceData.details.length > 128) presenceData.details = presenceData.details.substring(0, 128);
+            if (presenceData.state?.length > 128) presenceData.state = presenceData.state.substring(0, 128);
+            else if (presenceData.state?.length === 0) delete presenceData.state;
         }, 15);
     } else {
         const interval = setInterval(() => {
-            if(disconnected) return clearInterval(interval);
-            if(!presenceData.details || !config.get("show")) return rpc.clearActivity();
-            if(presenceData.details?.length > 128) presenceData.details = presenceData.details.substring(0,128);
-            if(presenceData.state?.length > 128) presenceData.state = presenceData.state.substring(0,128);
-            else if(presenceData.state?.length === 0) delete presenceData.state;
+            if (disconnected) return clearInterval(interval);
+            if (!presenceData.details || !config.get("show")) return rpc.clearActivity();
+            if (presenceData.details?.length > 128) presenceData.details = presenceData.details.substring(0, 128);
+            if (presenceData.state?.length > 128) presenceData.state = presenceData.state.substring(0, 128);
+            else if (presenceData.state?.length === 0) delete presenceData.state;
         }, 15);
     }
 }
