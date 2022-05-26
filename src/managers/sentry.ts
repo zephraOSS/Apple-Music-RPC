284c23ceefb5c112464f7c549714124962cd46eb
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-05-26 13:08:11 +0200

    Add environment

--- src/managers/sentry.ts
+++ src/managers/sentry.ts
@@ -6,7 +6,10 @@ export function init() {
     if (!app.isPackaged) return log.warn("[SENTRY]", "Sentry is disabled");
 
     Sentry.init({
-        dsn: "https://5da6c0e155b9475299808dd3daa0cf93@o1209127.ingest.sentry.io/6402650"
+        dsn: "https://5da6c0e155b9475299808dd3daa0cf93@o1209127.ingest.sentry.io/6402650",
+        environment: /-[a-z]*/g.test(app.getVersion())
+            ? app.getVersion().replace(/^(\d+)\.(\d+)\.(\d+)-/g, "")
+            : "production"
     });
 
     log.info("[SENTRY]", "Sentry initialized");
