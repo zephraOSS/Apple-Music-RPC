fef8ec7218bc890af27f8bd76c658a32bf6a326c
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-10-10 22:04:44 +0200

    [AMR-10] fix: tray quit button not working

--- src/managers/tray.ts
+++ src/managers/tray.ts
@@ -49,7 +49,7 @@ export class TrayManager {
             {
                 label: "Quit",
                 click() {
-                    app.quit();
+                    app.exit();
                 }
             }
         ]);
