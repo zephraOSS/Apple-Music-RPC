fa1a528f434211c815d9719c0b719b9ca872bef3
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-11-29 22:59:36 +0100

    [AMR-20] fix: use try

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -2,6 +2,8 @@ import { JSONParse } from "./json";
 
 import fetch from "node-fetch";
 
+import * as log from "electron-log";
+
 /**
  * @param path the path to the file
  * @param host host url is https://www.zephra.cloud/zaphy/api/
@@ -10,11 +12,15 @@ export async function apiRequest(
     path: string,
     host: string = "https://www.zephra.cloud/zaphy/api/"
 ) {
-    const res = await fetch(`${host}${path}`, {
-        headers: {
-            "User-Agent": "AMRPC"
-        }
-    });
+    try {
+        const res = await fetch(host + path, {
+            headers: {
+                "User-Agent": "AMRPC"
+            }
+        });
 
-    return JSONParse(await res.text());
+        return JSONParse(await res.text());
+    } catch (e) {
+        log.error("[apiRequest]", "Error:", e);
+    }
 }
